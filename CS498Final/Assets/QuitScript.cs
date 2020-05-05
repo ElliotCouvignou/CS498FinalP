28419c373d5792095db11af32ec59115df7c0129
jmrudolph3 <jmrudolph3@gmail.com>
2020-05-05 18:44:32 -0500

    Pause Menu Fixed
    Buttons should now work

--- CS498Final/Assets/QuitScript.cs
+++ CS498Final/Assets/QuitScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuitScript : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class QuitScript : MonoBehaviour
     {
       Debug.Log("QUIT");
       Application.Quit();
+      SceneManager.LoadScene("Menu");
     }
     // Update is called once per frame
     void Update()
